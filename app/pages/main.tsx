33f50323a70d6c6a2513c8b64181f7b8430c244c
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2024-01-10 15:40:39 +0600

    cmt

--- app/pages/main.tsx
+++ app/pages/main.tsx
@@ -1,18 +1,14 @@
 import React, {useState} from 'react';
 import styles from './pages.module.css';
-import Header from '../components/Header/Header';
 import ChatPage from './Chatbot/page';
 
 const Main: React.FC = () => {
     const [active, setActive] = useState(true);
 
     return (
-
-            <div className={styles.mainWrapper}>
-                <Header/>
-
-                <ChatPage active={active} setActive={setActive}/>
-            </div>
+        <div className={styles.mainWrapper}>
+            <ChatPage active={active} setActive={setActive}/>
+        </div>
     );
 };
 
